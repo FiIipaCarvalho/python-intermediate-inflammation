5ea07d584c816f9936a47191ed85c4e720a6826d
Filipa Carvalho <pipa85@gmail.com>
2025-01-15 15:50:40 +0000

    more style fixes applied

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -6,16 +6,16 @@ import argparse
 from inflammation import models, views
 
 
-def main(args):
+def main(argmts):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    in_files = args.infiles
+    in_files = argmts.infiles
     if not isinstance(in_files, list):
-        in_files = [args.infiles]
+        in_files = [argmts.infiles]
 
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
